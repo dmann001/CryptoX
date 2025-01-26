821ff3889bf3fdc0fb606d26bedadee2781d263d
Dhruv <dhruvmann040@gmail.com>
2025-01-26 18:17:35 -0500

    optimized mobile ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,48 +1,84 @@
-import React, { useContext } from 'react'
-import './Navbar.css'
-import logo from '../../assets/blockchain2.png'
-import arrow from '../../assets/arrow.png'
-import { CoinContext } from '../../context/CoinContext'
+import React, { useContext, useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/blockchain2.png';
+import arrow from '../../assets/arrow.png';
+import { CoinContext } from '../../context/CoinContext';
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+  const [showMessage, setShowMessage] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const {setCurrency} = useContext(CoinContext)
-  const currencyHandler = (event)=>{
-    switch (event.target.value){
-      case "usd":{
-        setCurrency({name:"usd", symbol:"$"});
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case 'usd': {
+        setCurrency({ name: 'usd', symbol: '$' });
         break;
       }
-      case "cad" :{
-        setCurrency({name:"cad", symbol:"CA$"});
+      case 'cad': {
+        setCurrency({ name: 'cad', symbol: 'CA$' });
         break;
       }
-      default:{
-        setCurrency({name:"usd", symbol:"$"});
+      default: {
+        setCurrency({ name: 'usd', symbol: '$' });
         break;
       }
     }
-  }
+  };
+
+  const handleSignupClick = () => {
+    setShowMessage(true);
+    setTimeout(() => setShowMessage(false), 3000);
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="navbar">
-        <span className='logo'>
-            <a href="/"><img src={logo} alt="" />CryptoX</a></span>
-        
-        <ul>
-            {/* <li>Dashboard</li> */}
-            <li>Markets</li>
-            {/* <li>Pricing</li>
-            <li>Blog</li> */}
-        </ul>
-        <div className="nav-right">
-            <select onChange={currencyHandler}>
-                <option value="usd">USD</option>
-                <option value="cad">CAD</option>
-                </select>
-                <button>Sign up<img src={arrow} alt=" "/></button>
-        </div>
-    </div>
-  )
-}
+      <div className="logo">
+        <a href="/"><img src={logo} alt="" />CryptoX</a>
+        <button 
+          className="menu-toggle" 
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
 
-export default Navbar
+      <div className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
+        <ul>
+          {/* <li>Dashboard</li> */}
+          {/* <li>Markets</li> */}
+          {/* <li>Pricing</li> */}
+          {/* <li>Blog</li> */}
+        </ul>
+
+        <div className="nav-right">
+          <select onChange={currencyHandler}>
+            <option value="usd">USD</option>
+            <option value="cad">CAD</option>
+          </select>
+          <div className="signup-container">
+            <button onClick={handleSignupClick}>
+              Sign up
+              <img src={arrow} alt=" " />
+            </button>
+
+            {showMessage && (
+              <div className="coming-soon-message">
+                Coming Soon!
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
