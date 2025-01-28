1d689357ea0ffe7c25523fb0a8b960a7fb688206
Dhruv <dhruvmann040@gmail.com>
2025-01-28 01:57:40 -0500

    added more ui changes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div>
+      <p className='Footer'>
+      Copyright (c) 2025 CryptoX
+      </p>
+    </div>
+  )
+}
+
+export default Footer
