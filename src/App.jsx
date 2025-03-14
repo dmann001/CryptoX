9c916ff90e198f5159d2080479dd49db705f5ab3
Dhruv <dhruvmann040@gmail.com>
2025-03-13 22:08:33 -0400

    fixed router error

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,21 @@ import Coin from './pages/Coin/Coin'
 import Footer from './components/Footer/Footer'
 import { AnalyticsProvider } from './providers/AnalyticsProvider'
 import { SpeedInsights } from "@vercel/speed-insights/react"
-import { BrowserRouter } from 'react-router-dom'
 import CoinContextProvider from './context/CoinContext'
 
 const App = () => {
   return (
     <AnalyticsProvider>
       <div className='app'>
-        <BrowserRouter>
-          <CoinContextProvider>
-            <Navbar />
-            <Routes>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/coin/:coinId' element={<Coin/>}/>
-            </Routes>
-            <Footer />
-            <SpeedInsights />
-          </CoinContextProvider>
-        </BrowserRouter>
+        <CoinContextProvider>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/coin/:coinId' element={<Coin/>}/>
+          </Routes>
+          <Footer />
+          <SpeedInsights />
+        </CoinContextProvider>
       </div>
     </AnalyticsProvider>
   )
